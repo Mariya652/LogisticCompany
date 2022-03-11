01f3281dbc642db35d6a1abf7a1240e0da131a65
Mariya Borshykova <mariyaborshykova@gmail.com>
2022-03-11 11:54:49 +0200

    parcel controller end

--- src/main/java/com/informatics/LogisticCompany/webControllers/api/ParcelController.java
+++ src/main/java/com/informatics/LogisticCompany/webControllers/api/ParcelController.java
@@ -6,6 +6,8 @@ import com.informatics.LogisticCompany.services.ParcelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -44,6 +46,29 @@ public class ParcelController {
     @GetMapping(value = "api/parcelByEmployee/{employeeId}")
     public List<Parcel> findByEmployeeId(@PathVariable("employeeId") Long id){
         return parcelService.findByEmployeeId(id);
+    }
+
+    @GetMapping(value = "api/parcelNotReceived")
+    public List<Parcel> findAllByReceiveDateGreaterThan(){
+        return parcelService.findAllByReceiveDateGreaterThan();
+    }
+
+    @GetMapping(value = "api/parcelBySenderId/{id}")
+    public List<Parcel> findBySenderId(@PathVariable("id") Long id){
+        return parcelService.findBySenderId(id);
+    }
+
+    @GetMapping(value = "api/parcelByReceiverId/{id}")
+    public List<Parcel> findByReceiverId(@PathVariable("id") Long id){
+        return parcelService.findByReceiverId(id);
+    }
+
+    @GetMapping(value = "api/parcelBetweenDate/{sendDate}/{receiveDate}")
+    public List<Parcel> findBySendDateGreaterThanEqualAndReceiveDateLessThanEqual(@PathVariable("sendDate") String sendDate, @PathVariable("receiveDate") String receiveDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate sendDateLocal = LocalDate.parse(sendDate, formatter);
+        LocalDate receiveDateLocal = LocalDate.parse(receiveDate, formatter);
+        return parcelService.findBySendDateGreaterThanEqualAndReceiveDateLessThanEqual(sendDateLocal, receiveDateLocal);
     }
 
 }
